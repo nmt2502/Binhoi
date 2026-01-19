2368ed1c5918f2565a179ab590475e8248c9c076
nmt2502 <anhlatuan2502@gmail.com>
2026-01-20 05:12:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
-require("./worker"); // KÍCH HOẠT NGẦM
+require("./worker"); // chạy ngầm
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// ROOT
 app.get("/", (req, res) => {
-  res.send("Bot đang chạy ngầm...");
+  res.send("Bot đang chạy ngầm OK");
+});
+
+// PING cho UptimeRobot
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
 app.listen(PORT, () => {
-  console.log("Server online");
+  console.log("Server online port", PORT);
 });
